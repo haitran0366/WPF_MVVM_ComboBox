500601a40ba9cf894a1f1f8b86c5ba5ed1d657b0
haitran0366 <haitran_usa@yahoo.com>
2021-08-22 11:57:16 -0700

    Add project files.

---
+++ GreetingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVVM_ComboBox
+{
+    public class GreetingModel
+    {
+        public int countryID { get; set; }
+        public string countryName { get; set; }
+        public string greeting { get; set; }
+    }
+}
---
+++ GreetingViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MVVM_ComboBox
+{
+    public class GreetingViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string name ="")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private string hello;
+        public string Hello 
+        {
+            get { return hello; }
+            set
+            {
+                hello = value;
+                NotifyPropertyChanged("Hello");
+            }
+        }
+
+        public List<GreetingModel> countryList;
+        public List<GreetingModel> CountryList 
+        {
+            get { return countryList; }
+            set
+            {
+                countryList = value;
+            }
+        }
+
+        private GreetingModel selectedCountry;
+        public GreetingModel SelectedCountry 
+        {
+            get { return selectedCountry; }
+            set
+            {
+                selectedCountry = value;
+                NotifyPropertyChanged("SelectedCountry");
+                Hello = SelectedCountry.greeting;
+            }
+        }
+
+        public GreetingViewModel()
+        {
+            CountryList = new List<GreetingModel>
+            {
+                new GreetingModel {countryID = 1, countryName="US", greeting ="Hello"},
+                new GreetingModel{countryID=2,countryName="VietNam", greeting="Xin chào"},
+                new GreetingModel{countryID=3,countryName="Mexico", greeting ="Holla"}
+            };
+            SelectedCountry = CountryList[0]; 
+
+        }
+    }
+}
